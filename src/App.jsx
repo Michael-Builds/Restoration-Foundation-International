83ffeadcb9f5beb76d7444b3a52bea051f07fea5
Kabanda Kpanti Michael <michaelkpantiramp@gmail.com>
2023-10-04 03:08:29 +0000

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import './index.css';
+
+function App() {
+
+  return (
+    <>
+      <div>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <div className="font-quicksand">
+          <h1 className="text-2xl font-quicksand">Welcome to My Vite React App</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
