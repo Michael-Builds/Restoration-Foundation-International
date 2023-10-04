cf64c02bcd651bd87451de34dfee49a7e621126b
Kabanda Kpanti Michael <michaelkpantiramp@gmail.com>
2023-10-04 03:14:16 +0000

    Added Font Customization

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <div className="font-quicksand">
-          <h1 className="text-2xl font-quicksand">Welcome to My Vite React App</h1>
+          <h1 className="text-3xl font-quicksand">Welcome to My Vite React App</h1>
         </div>
       </div>
     </>
