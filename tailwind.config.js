3ef977215588fde21603cfa48887f35abeb6df6d
Kabanda Kpanti Michael <michaelkpantiramp@gmail.com>
2023-10-15 18:00:37 +0000

    Configured Font Yeseva

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
         quicksand: ['Quicksand', 'sans-serif'],
+        yeseva: ['Yeseva One', 'cursive'],
       },
     },
   },
   plugins: [],
 }
-
