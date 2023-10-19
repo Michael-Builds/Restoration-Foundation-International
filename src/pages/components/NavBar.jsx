662144455a969001a4a670466464d1039e6e7355
Kabanda Kpanti Michael <michaelkpantiramp@gmail.com>
2023-10-19 13:26:16 +0000

    Updated NavBar Mobile View

--- src/pages/components/NavBar.jsx
+++ src/pages/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { IoIosArrowDown } from 'react-icons/io';
-import { BiSolidDollarCircle } from 'react-icons/bi';
+// import { IoIosArrowDown } from 'react-icons/io';
+import { BiSolidDollarCircle, BiPlus } from 'react-icons/bi';
 import Logo from '../../assets/logo.png';
 import {
     Navbar,
@@ -14,7 +14,26 @@ import { NavLink } from 'react-router-dom';
 const Navigation = () => {
 
     const [openNav, setOpenNav] = React.useState(false);
+    const [eventDropdownOpen, setEventDropdownOpen] = React.useState(false);
+    const [aboutDropdownOpen, setAboutDropDownOpen] = React.useState(false);
+
     const showDonateButton = window.innerWidth >= 960; // Check screen width for mobile view
+
+    const handleEventMouseEnter = () => {
+        setEventDropdownOpen(true);
+    };
+
+    const handleAboutMouseEnter = () => {
+        setAboutDropDownOpen(true);
+    }
+
+    const handleEventMouseLeave = () => {
+        setEventDropdownOpen(false);
+    };
+
+    const handleAboutMouseLeave = () => {
+        setAboutDropDownOpen(false);
+    }
 
     React.useEffect(() => {
         window.addEventListener(
@@ -32,49 +51,118 @@ const Navigation = () => {
                 color="blue-gray"
                 className="p-1 font-normal font-quicksand"
             >
-                <NavLink to="/" className="flex items-center text-1xl text-gray-500 hover:text-yellow-500 font-quicksand">
+                <NavLink to="/" className="flex items-center font-medium text-lg text-gray-500 hover:text-yellow-500 font-quicksand">
                     Home
                 </NavLink>
             </Typography>
-            <Typography
-                as="li"
-                variant="small"
-                color="blue-gray"
-                className="p-1 font-normal font-quicksand"
+            <li
+                className="relative"
+                onMouseEnter={openNav ? null : handleAboutMouseEnter}
+                onMouseLeave={openNav ? null : handleAboutMouseLeave}
             >
-                <NavLink to="/about" className="flex items-center text-1xl text-gray-500 hover:text-yellow-500 font-quicksand">
-                    About
-                </NavLink>
-            </Typography>
+                <Typography
+                    as="div"
+                    variant="small"
+                    color="blue-gray"
+                    className={`p-1 font-normal font-quicksand ${aboutDropdownOpen ? "text-gray-500" : "text-gray-500"
+                        }`}
+                >
+                    <div className="flex items-center justify-between font-medium text-lg cursor-pointer">
+                        About
+                        {openNav ? ( // Conditionally render the plus icon
+                            <span
+                                className="ml-3 text-yellow-500 cursor-pointer"
+                                onClick={() => setAboutDropDownOpen(!aboutDropdownOpen)}
+                            >
+                                <BiPlus />
+                            </span>
+                        ) : null}
+                    </div>
+                    {aboutDropdownOpen && (
+                        <ul className={`absolute top-8 left-0 z-10 bg-white justify-center text-gray-600 p-4 w-[10rem] lg:border-b-4 lg:border-yellow-500 rounded-md ${openNav ? '' : 'pointer-events-none'}`}>
+                            <li>
+                                <NavLink
+                                    to="/about"
+                                    className={`block p-2 lg:hover:bg-white lg:hover:border-b-2 rounded-md lg:border-yellow-500 font-medium text-md mt-4 ${openNav ? 'lg:hover:text-yellow-500' : ''}`}
+                                >
+                                    About Us
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/service2"
+                                    className={`block p-2 lg:hover:bg-white lg:hover:border-b-2 rounded-md lg:border-yellow-500 font-medium text-md mt-4 ${openNav ? 'lg:hover:text-yellow-500' : ''}`}
+                                >
+                                    About Founder
+                                </NavLink>
+                            </li>
+                        </ul>
+                    )}
+                </Typography>
+            </li>
             <Typography
                 as="li"
                 variant="small"
                 color="blue-gray"
                 className="p-1 font-normal font-quicksand "
             >
-                <NavLink to="/causes" className="flex items-center text-1xl text-gray-500 hover:text-yellow-500 font-quicksand">
+                <NavLink to="/causes" className="flex items-center font-medium text-lg text-gray-500 hover:text-yellow-500 font-quicksand">
                     Causes
                 </NavLink>
             </Typography>
-            <Typography
-                as="li"
-                variant="small"
-                color="blue-gray"
-                className="p-1 font-normal font-quicksand"
+            <li
+                className="relative"
+                onMouseEnter={openNav ? null : handleEventMouseEnter}
+                onMouseLeave={openNav ? null : handleEventMouseLeave}
             >
-                <NavLink to="/events" className="flex text-16 items-center text-gray-500 hover:text-yellow-500 font-quicksand">
-                    Events
-                </NavLink>
-            </Typography>
+                <Typography
+                    as="div"
+                    variant="small"
+                    color="blue-gray"
+                    className={`p-1 font-normal font-quicksand ${eventDropdownOpen ? "text-gray-500" : "text-gray-500"
+                        }`}
+                >
+                    <div className="flex items-center justify-between font-medium text-lg cursor-pointer">
+                        Events
+                        {openNav ? ( // Conditionally render the plus icon
+                            <span
+                                className="ml-3 text-yellow-500 cursor-pointer"
+                                onClick={() => setEventDropdownOpen(!eventDropdownOpen)}
+                            >
+                                <BiPlus />
+                            </span>
+                        ) : null}
+                    </div>
+                    {eventDropdownOpen && (
+                        <ul className={`absolute top-8 left-0 z-10 bg-white justify-center items-center text-gray-600 p-4 w-[10rem] border-b-4 border-yellow-500 rounded-md`}>
+                            <li>
+                                <NavLink
+                                    to="/service1"
+                                    className={`block p-2 hover:bg-white hover:border-b-2 rounded-md border-yellow-500 font-medium text-md mt-4 hover:text-yellow-500`}
+                                >
+                                    Recent
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/service2"
+                                    className={`block p-2 hover:bg-white hover:border-b-2 rounded-md border-yellow-500 font-medium text-md mt-4 hover:text-yellow-500`}
+                                >
+                                    Upcoming ....
+                                </NavLink>
+                            </li>
+                        </ul>
+                    )}
+                </Typography>
+            </li>
             <Typography
                 as="li"
                 variant="small"
                 color="blue-gray"
                 className="p-1 font-normal font-quicksand flex items-center"
             >
-                <NavLink to="/service" className="flex items-center text-16 text-gray-500 hover:text-yellow-500 font-quicksand">
+                <NavLink to="/service" className="flex items-center font-medium text-lg text-gray-500 hover:text-yellow-500 font-quicksand">
                     Services
-                    <IoIosArrowDown className="text-gray-500 ml-1 hover:text-yellow-500" />
                 </NavLink>
             </Typography>
 
@@ -85,7 +173,7 @@ const Navigation = () => {
                 color="blue-gray"
                 className="p-1 font-normal font-quicksand"
             >
-                <NavLink to="/blogs" className="flex items-center text-16 text-gray-500 hover:text-yellow-500 font-quicksand">
+                <NavLink to="/blogs" className="flex items-center font-medium text-lg text-gray-500 hover:text-yellow-500 font-quicksand">
                     Blogs
                 </NavLink>
             </Typography>
